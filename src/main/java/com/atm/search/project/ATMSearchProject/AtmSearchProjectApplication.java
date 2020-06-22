64d1c35e47ec6f7493b6e006e8c33fa304554cd9
amanca <snake_lr@hotmail.com>
2020-06-22 14:34:36 -0300

    update pom

--- src/main/java/com/atm/search/project/ATMSearchProject/AtmSearchProjectApplication.java
+++ src/main/java/com/atm/search/project/ATMSearchProject/AtmSearchProjectApplication.java
@@ -2,12 +2,19 @@ package com.atm.search.project.ATMSearchProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class AtmSearchProjectApplication {
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class AtmSearchProjectApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AtmSearchProjectApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtmSearchProjectApplication.class, args);
 	}
-
 }
